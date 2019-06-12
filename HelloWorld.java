6fbe5fb3f27e149751be16035f7eef9cace0ac84
clickhirphae <hpclickcomputerhope@gmail.com>
2019-06-12 14:35:04 -0400

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+
+
+        public static void main(String[] args) {
+            System.out.println("Hello, World!");
+            System.out.println("Today is June 12, 2019. I am alive!");
+            System.out.printf("My name is Hirpha.");
+        }
+    }
+
